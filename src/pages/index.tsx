eefa71839357af59f9e0ead7eba72a3c5545fbb8
laclac102 <vpha0885@uni.sydney.edu.au>
2025-07-08 15:32:53 +1000

    lint + Home unittest

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
-import Layout from "../components/Layout";
+// src/pages/index.tsx
 export default function Home() {
   return (
-    <Layout />
+    <main>
+      <h1>Welcome to BindFlow UI</h1>
+    </main>
   );
 }
