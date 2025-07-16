1bb520ba142e181980e4bca2d03b4efe26037a47
laclac102 <phanvothungabiotech@gmail.com>
2025-07-16 15:00:16 +1000

    improve lint

--- src/components/SideBarItems.tsx
+++ src/components/SideBarItems.tsx
@@ -1,6 +1,9 @@
-import { List, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
-import Link from "@mui/material/Link";
-import NextLink from "next/link";
+import {
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText
+} from "@mui/material";
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 import DeviceHubOutlinedIcon from "@mui/icons-material/DeviceHubOutlined";
 import WorkOutlineOutlinedIcon from "@mui/icons-material/WorkOutlineOutlined";
@@ -11,79 +14,80 @@ import ContactSupportOutlinedIcon from "@mui/icons-material/ContactSupportOutlin
 import { useRouter } from "next/router";
 import { useWorkflows } from "@/context/DBContext";
 
-
 export default function SideBarItems() {
-    const router = useRouter();
-    const context = useWorkflows();
-    const themes = context?.themes;
-    const navItems = [
-      {
-        href: "/",
-        text: "Home",
-        icon: <HomeOutlinedIcon />,
-        childs: themes
-      },
-      {
-        href: "/preconfig",
-        icon: <DeviceHubOutlinedIcon />,
-        text: "Pre-config Workflow"
-      },
-      { href: "/jobs", icon: <WorkOutlineOutlinedIcon />, text: "Jobs" },
-      { href: "/about", icon: <InfoOutlinedIcon />, text: "About" },
-      {
-        href: "/events",
-        icon: <InsertInvitationOutlinedIcon />,
-        text: "Workshops & Events"
-      },
-      {
-        href: "/contact",
-        icon: <ContactSupportOutlinedIcon />,
-        text: "Support/FAQ"
-      },
-      {
-        href: "/login",
-        icon: <PersonOutlineOutlinedIcon />,
-        text: "Log in"
-      }
-    ]; 
+  const router = useRouter();
+  const context = useWorkflows();
+  const themes = context?.themes;
+  const navItems = [
+    {
+      href: "/",
+      text: "Home",
+      icon: <HomeOutlinedIcon />,
+      childs: themes
+    },
+    {
+      href: "/preconfig",
+      icon: <DeviceHubOutlinedIcon />,
+      text: "Pre-config Workflow"
+    },
+    { href: "/jobs", icon: <WorkOutlineOutlinedIcon />, text: "Jobs" },
+    { href: "/about", icon: <InfoOutlinedIcon />, text: "About" },
+    {
+      href: "/events",
+      icon: <InsertInvitationOutlinedIcon />,
+      text: "Workshops & Events"
+    },
+    {
+      href: "/contact",
+      icon: <ContactSupportOutlinedIcon />,
+      text: "Support/FAQ"
+    },
+    {
+      href: "/login",
+      icon: <PersonOutlineOutlinedIcon />,
+      text: "Log in"
+    }
+  ];
 
-    return (
-      <List>
-        {navItems.map(({ href, icon, text, childs }) => (
-          <>
-            <ListItemButton
-              onClick={() => {
-                router.push({
-                  pathname: href
-                });
-              }}
-            >
-              <ListItemIcon>{icon}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-            {childs &&
-              childs.map((child: string) => {
-                return (
-                  // Generate themes list under Home
-                  <ListItemButton
-                    onClick={() => {
-                      router.push({
-                        pathname: "/workflowThemes",
-                        query: { id: child }
-                      });
-                    }}
-                  >
-                    <ListItemIcon></ListItemIcon>
-                    <ListItemText
-                      primary={child
-                        .replace(/_/g, " ")
-                        .replace(/\b\w/g, (char) => char.toUpperCase())}
-                    />
-                  </ListItemButton>
-                );
-              })}
-          </>
-        ))}
-      </List>
-    );
+  return (
+    <List>
+      {navItems.map(({ href, icon, text, childs }) => (
+        <>
+          <ListItemButton
+            key={href}
+            onClick={() => {
+              router.push({
+                pathname: href
+              });
+            }}
+          >
+            <ListItemIcon>{icon}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItemButton>
+          {childs &&
+            childs.map((child: string) => {
+              return (
+                // Generate themes list under Home
+                <ListItemButton
+                  key={child}
+                  onClick={() => {
+                    router.push({
+                      pathname: "/workflowThemes",
+                      query: { id: child }
+                    });
+                  }}
+                >
+                  <ListItemIcon></ListItemIcon>
+                  <ListItemText
+                    primary={child
+                      .replace(/_/g, " ")
+                      .replace(/\b\w/g, (char) => char.toUpperCase())}
+                  />
+                </ListItemButton>
+              );
+            })}
+        </>
+      ))}
+    </List>
+  );
 }
