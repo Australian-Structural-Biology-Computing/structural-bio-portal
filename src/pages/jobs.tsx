08ee365e6cc0e11cdd64484d57f94ba131b125ab
laclac102 <phanvothungabiotech@gmail.com>
2025-07-24 13:13:55 +1000

    set status for Workflow cancel

--- src/pages/jobs.tsx
+++ src/pages/jobs.tsx
@@ -2,7 +2,16 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import { DataGrid, GridColDef, GridRowParams } from "@mui/x-data-grid";
-import { Button } from "@mui/material";
+import {
+  Alert,
+  Backdrop,
+  Button,
+  CircularProgress,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle
+} from "@mui/material";
 import { listRuns } from "@/controllers/listRuns";
 import { RunInfo } from "@/models/workflow";
 import { cancelWorkflow } from "@/controllers/cancelWorkflow";
@@ -10,6 +19,13 @@ import Router from "next/router";
 
 export default function MyRuns() {
   const [runs, setRuns] = React.useState<RunInfo[]>([]);
+  const [status, setStatus] = React.useState<
+    "idle" | "loading" | "done" | "error"
+  >("idle");
+  const [canceling, setCanceling] = React.useState(false);
+  const [openDialog, setOpenDialog] = React.useState(false);
+  const [selectedId, setSelectedId] = React.useState<string | null>(null);
+  const [errorMsg, setErrorMsg] = React.useState("");
   const handleRowClick = (params: GridRowParams) => {
     const id = params.row.id;
     Router.push({ pathname: "/jobs/results", query: { id: id } });
@@ -29,14 +45,7 @@ export default function MyRuns() {
     }));
     setRuns(Runs);
   };
-  const handleCancel = async (workflowId: string) => {
-    try {
-      await cancelWorkflow(workflowId);
-      await fetchRuns();
-    } catch (error) {
-      console.error("Cancel failed:", error);
-    }
-  };
+
   React.useEffect(() => {
     fetchRuns();
   }, []);
@@ -76,8 +85,10 @@ export default function MyRuns() {
           variant="outlined"
           size="small"
           disabled={!["SUBMITTED", "RUNNING"].includes(params.row.status)}
-          onClick={() => {
-            handleCancel(params.row.id);
+          onClick={(e) => {
+            e.stopPropagation();
+            setSelectedId(params.row.id);
+            setOpenDialog(true);
           }}
         >
           Cancel
@@ -86,21 +97,64 @@ export default function MyRuns() {
     }
   ];
   return (
-    <Box sx={{ hieght: "100vh", width: "100%" }}>
-      <DataGrid
-        rows={runs}
-        columns={columns}
-        onRowClick={handleRowClick}
-        initialState={{
-          pagination: {
-            paginationModel: {
-              pageSize: 10
-            }
-          }
-        }}
-        pageSizeOptions={[10]}
-        checkboxSelection
-      />
+    <Box sx={{ height: "80vh", width: "100%" }}>
+      <Backdrop
+        open={canceling}
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>{" "}
+      {status === "error" && <Alert>Error: {errorMsg}</Alert>}
+      {(status === "idle" || "done") && (
+        <>
+          <DataGrid
+            rows={runs}
+            columns={columns}
+            onRowClick={handleRowClick}
+            initialState={{
+              pagination: {
+                paginationModel: {
+                  pageSize: 10
+                }
+              }
+            }}
+            pageSizeOptions={[10]}
+            checkboxSelection
+          />
+          <Dialog open={openDialog} onClose={() => setOpenDialog(false)}>
+            <DialogTitle>Confirm Cancellation</DialogTitle>
+            <DialogContent>
+              Are you sure you want to cancel this workflow?
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={() => setOpenDialog(false)}>No</Button>
+              <Button
+                onClick={async () => {
+                  if (selectedId) {
+                    setCanceling(true);
+                    try {
+                      await cancelWorkflow(selectedId);
+                      await fetchRuns();
+                      setStatus("done");
+                    } catch (error) {
+                      console.error("Cancel failed:", error);
+                      setStatus("error");
+                      setErrorMsg(`ERROR: ${error}`);
+                    } finally {
+                      setSelectedId(null);
+                      setOpenDialog(false);
+                      setCanceling(false);
+                    }
+                  }
+                }}
+                color="info"
+              >
+                Yes, Cancel
+              </Button>
+            </DialogActions>
+          </Dialog>
+        </>
+      )}
     </Box>
   );
 }
